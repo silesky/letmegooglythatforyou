9f3d7e12bc8284576d764998ba4945461baf2f05
Seth Silesky <seth.silesky@gmail.com>
2017-12-12 13:39:04 -0600

    tweak title

--- server/app.py
+++ server/app.py
@@ -375,7 +375,10 @@ async def get_image_slack(req):
             if json_res['ok']:
                 print(json(json_res))
                 user = json_res['profile']
-                display_name = '@' + user['display_name']
+                if 'display_name' in user:
+                    display_name = '@' + user['display_name']
+                else:
+                    display_name = user['real_name']
                 avatar_url = user.get('image_512') or user.get('image_192')
                 if not avatar_url:
                     return json('No suitable width image in requested user\'s profile.')
